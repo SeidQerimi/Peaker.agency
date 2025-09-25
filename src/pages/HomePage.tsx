cb6bc2914331c2355542b8116d559ae05ec497f5
Seidi <seidqerimi4@gmail.com>
2025-09-25 09:43:11 +0200

    krijova nje test page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Test from "../components/Test";
 
 const HomePage = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Test/>
     </div>
   );
 };
