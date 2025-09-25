870c4e5d6ff635190ff250a39e82b2e74ce93036
Seid Qerimi <seidqerimi4@gmail.com>
2025-09-25 09:46:08 +0200

    Update Test.tsx

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -5,7 +5,14 @@ const Test: React.FC = () => {
     <div className="flex h-screen items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold text-blue-600">
         Hello from Test.tsx 🚀
+        TEST
+        TEST
+        TEST
+        TEST
       </h1>
+      <p className="flex h-screen items-center justify-center bg-red-300">
+        Test paragraph
+      </p>
     </div>
   );
 };
